3d8d04e929a1e41a6910de40a170e8384904ee1a
oscar-linares <oscar.linares@airbnb.com>
2019-11-02 23:13:15 -0400

    add command line input

--- branch_demo/namegame.py
+++ branch_demo/namegame.py
@@ -1,4 +1,11 @@
-name = input('Enter your name: ')
+import sys
+
+print(sys.argv)
+
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+else:
+    name = input('Enter your name: ')
 
 for i in range(len(name), 0, -1):  # prints names minus on letter in every pass
     print(name[0:i], end= ' ')
