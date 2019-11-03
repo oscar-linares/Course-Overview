4d95b0a964b8a8f317c5e9843bd166d4f4904765
oscar-linares <oscar.linares@airbnb.com>
2019-11-02 23:03:43 -0400

    added spread-out letters

--- branch_demo/namegame.py
+++ branch_demo/namegame.py
@@ -1,4 +1,7 @@
 name = input('Enter your name: ')
 
-for i in range(len(name), 0, -1):
-    print(name[0:i])
+for i in range(len(name), 0, -1):  # prints names minus on letter in every pass
+    print(name[0:i], end= ' ')
+    for j in range(i, len(name)):  # print letter after space
+        print(' ' * (j-i) + name[j], end= '')
+    print('')  # print space
